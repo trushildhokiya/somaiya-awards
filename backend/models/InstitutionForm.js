1d505524d219afe8e0725ffe64012485e1d51a3b
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-06-05 11:59:32 +0530

    created institution form schema

--- backend/models/InstitutionForm.js
+++ backend/models/InstitutionForm.js
@@ -1,6 +1,251 @@
 module.exports = (sequelize,dataTypes)=>{
 
-    const OutstandingInstitution = sequelize.define("Outstanding Institution",{
+    const OutstandingInstitution = sequelize.define("OutstandingInstitution",{
+        email_id:{
+            type: dataTypes.STRING,
+            allowNull : false
+        },
         
+        nomination_category:{
+            type:dataTypes.STRING,
+            allowNull: false
+        },
+
+        institutuion_name:{
+            type: dataTypes.STRING,
+            allowNull : false
+        },
+
+        established_In : {
+            type: dataTypes.STRING,
+            allowNull : false
+        },
+
+        head_of_institution:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+
+        hoi_designation:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+
+        hoi_joining_date:{
+            type: dataTypes.DATEONLY,
+            allowNull: false
+        },
+
+        somaiya_mail_id:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+
+        contact_number:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+
+        q_01:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_02:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_03:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_04:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_05:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_06:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_07:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_08:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_09:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_10:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_11:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_12:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_13:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_14:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_14:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_16:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_17:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        institution_ratings:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+
+        q_18:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_19:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_20:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_21:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_22:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_23:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_24:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_25:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_26:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_27:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_28:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_29:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_30:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_31:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_32:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_33:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_34:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_35:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_36:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_37:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        q_38:{
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+
+        supportings :{
+            type: dataTypes.STRING,
+            allowNull: false
+        }
     })
+
+    return OutstandingInstitution;
 }
--- frontend/src/data/OutstandingInstForm.js
+++ frontend/src/data/OutstandingInstForm.js
@@ -14,7 +14,7 @@ const OutstandingInstForm= [
         title:"Please mark your nomination category",
         type:"radio",
         requiredStatus : true,
-        _name: "nomination-category",
+        _name: "nomination_category",
         hasOptions: true,
         options: ["Outstanding School", "Outstanding College"],
         page:1,
@@ -22,7 +22,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Name of Institution",
-        _name:"inst_name",
+        _name:"institution_name",
         type: 'text',
         hasOptions: false,
         requiredStatus : true,
@@ -31,7 +31,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Year of Establishment",
-        _name:"year_of_estb",
+        _name:"established_In",
         type: 'text',
         hasOptions : false,
         requiredStatus : true,
@@ -60,7 +60,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Date of Joining Of HOI",
-        _name:"date_of_join_hoi",
+        _name:"hoi_joining_date",
         type: 'date',
         hasOptions : false,
         requiredStatus : true,
@@ -69,7 +69,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Somaiya Email ID",
-        _name:"somaiaya_mail",
+        _name:"somaiya_mail_id",
         type: 'email',
         hasOptions : false,
         hasValidations:true,
@@ -91,7 +91,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Submit a short paragraph/ summary of why you believe your Institution qualifies for the award. (500 – 700 words only)",
-        _name:"qualify_reason",
+        _name:"q_01",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -100,7 +100,7 @@ const OutstandingInstForm= [
     },
     {
         title: "State the awards/certifications received from various national and international accrediting agencies such as AACSB, NAAC, NBA, Govt. Gradings, School boards and Govt. agencies. (500 words only)",
-        _name:"national_int_awards",
+        _name:"q_02",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -109,7 +109,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Has the institution secured any position in the National Institutional Ranking Framework (NIRF) by the HRD Ministry, Govt. of India. If yes, please specify details and attach relevant documents at the end of the form.(500 words only)",
-        _name:"position_nirf",
+        _name:"q_03",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -118,7 +118,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Has the institution secured any ranking in Industry Journals & periodicals,/ mainline magazines, newspapers. If yes, please specify details and attach relevant documents at the end of the form. (500 words only)",
-        _name:"journal_ranking",
+        _name:"q_04",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -127,7 +127,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Has the HOI or any of the faculty got nominated as Heads of Councils and Committees of outside agencies/ universities. If yes, please specify details and attach relevant documents at the end of the form. (500 words only)",
-        _name:"hoi_nominated",
+        _name:"q_05",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -136,7 +136,7 @@ const OutstandingInstForm= [
     },
     {
         title: "Mention the number and list of toppers (students) at University levels or Board levels (250 words only)",
-        _name:"toppers_list",
+        _name:"q_06",
         type: 'textarea',
         hasOptions : false,
         requiredStatus : true,
@@ -147,7 +147,7 @@ const OutstandingInstForm= [
         title:"Comment on the admissions status (total available student intake and vacant seats) and increasing trends for each year of your institution (250 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "admission_status",
+        _name: "q_07",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -156,7 +156,7 @@ const OutstandingInstForm= [
         title:"Specify about any grants received from outside agencies and Governmental bodies’ and about their utilization (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "grants_recieved",
+        _name: "q_08",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -165,7 +165,7 @@ const OutstandingInstForm= [
         title:"Mention the college festivals and events (about any unique events which has benefitted to the campus/society) (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "clg_fest_events",
+        _name: "q_09",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -174,7 +174,7 @@ const OutstandingInstForm= [
         title:"List the current and market oriented courses introduced this year. (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "courses_curr",
+        _name: "q_10",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -183,7 +183,7 @@ const OutstandingInstForm= [
         title:"Have you organized any national/ international level conferences/ seminar/ exhibition. Please attach a post-show report along with photographs at the end of this form (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "seminar_conf",
+        _name: "q_11",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -192,7 +192,7 @@ const OutstandingInstForm= [
         title:"List all the donations and scholarships received by your Institution (250 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "scholarship_donation",
+        _name: "q_12",
         hasOptions: false,
         page:2,
         fieldsPerLine:1
@@ -201,7 +201,7 @@ const OutstandingInstForm= [
         title:"Have you added any new infrastructure or facilities this year (750 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "new_infra",
+        _name: "q_13",
         hasOptions: false,
         page:3,
         fieldsPerLine:1
@@ -210,7 +210,7 @@ const OutstandingInstForm= [
         title:"What are the activities undertaken to maintain a hygienic environment within the building and external surroundings (750 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "hygenic_activities",
+        _name: "q_14",
         hasOptions: false,
         page:3,
         fieldsPerLine:1
@@ -219,7 +219,7 @@ const OutstandingInstForm= [
         title:"State measures taken to enhance the student experience in your institution (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "student_exp",
+        _name: "q_15",
         hasOptions: false,
         page:3,
         fieldsPerLine:1
@@ -228,7 +228,7 @@ const OutstandingInstForm= [
         title:"State the frequency of Safety procedures and processes such as fire drills, women’s safety, etc. being conducted. (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "safety_procedure",
+        _name: "q_16",
         hasOptions: false,
         page:3,
         fieldsPerLine:1
@@ -237,7 +237,7 @@ const OutstandingInstForm= [
         title:"State the Disaster Management system, Communication strategies with parents in case of emergencies, eg. floods, etc. (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "disaster_mang",
+        _name: "q_17",
         hasOptions: false,
         page:3,
         fieldsPerLine:1
@@ -256,7 +256,7 @@ const OutstandingInstForm= [
         title:"List the received external awards at state, national or international competitions in the following areas ● Cultural activities- music, dance, drama, debates ● Sports activities and competitions ● NCC / NSS ● External exams such as NTSE / Olympiads etc (1000 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "external_awards",
+        _name: "q_18",
         hasOptions: false,
         page:4,
         fieldsPerLine:1
@@ -265,7 +265,7 @@ const OutstandingInstForm= [
         title:"State the parental involvement and contribution for schools such as PTA etc (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "parental_envolvement",
+        _name: "q_19",
         hasOptions: false,
         page:4,
         fieldsPerLine:1
@@ -274,7 +274,7 @@ const OutstandingInstForm= [
         title:"List the Committees run by students and their significant achievements (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "student_committees",
+        _name: "q_20",
         hasOptions: false,
         page:4,
         fieldsPerLine:1
@@ -283,7 +283,7 @@ const OutstandingInstForm= [
         title:"List and provide a brief synopsis of the research projects (completed and ongoing) (750 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "research_projects",
+        _name: "q_21",
         hasOptions: false,
         page:5,
         fieldsPerLine:1
@@ -292,7 +292,7 @@ const OutstandingInstForm= [
         title:"State the faculty awards and recognitions from outside agencies (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "faculty_awards",
+        _name: "q_22",
         hasOptions: false,
         page:5,
         fieldsPerLine:1
@@ -301,7 +301,7 @@ const OutstandingInstForm= [
         title:"Provide details of faculty articles published in journals, magazines, books in both national and international publications (attach photocopies at the end of the form) (1000 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "articles_published",
+        _name: "q_23",
         hasOptions: false,
         page:5,
         fieldsPerLine:1
@@ -310,7 +310,7 @@ const OutstandingInstForm= [
         title:"List and provide a short synopsis of the total number of faculty participation and presentation of papers in state, national, international,  conferences/seminars organized by other institutions/agencies (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "faculty_participation",
+        _name: "q_24",
         hasOptions: false,
         page:5,
         fieldsPerLine:1
@@ -319,7 +319,7 @@ const OutstandingInstForm= [
         title:"State some significant improvements in Governance (processes & culture) (500 words only))",
         type:"textarea",
         requiredStatus : true,
-        _name: "governance_improve",
+        _name: "q_25",
         hasOptions: false,
         page:6,
         fieldsPerLine:1
@@ -328,7 +328,7 @@ const OutstandingInstForm= [
         title:"What are the processes and methodology used to share the Founder’s vision and mission within your institution (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "vision_share",
+        _name: "q_26",
         hasOptions: false,
         page:6,
         fieldsPerLine:1
@@ -337,7 +337,7 @@ const OutstandingInstForm= [
         title:"Describe the use of latest technology as teaching aids and teaching innovation methodology (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "latest_tech_aids",
+        _name: "q_27",
         hasOptions: false,
         page:6,
         fieldsPerLine:1
@@ -346,7 +346,7 @@ const OutstandingInstForm= [
         title:"List the Faculty & Staff Development programmes conducted vis a vis those identified (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "fS_dev_prog",
+        _name: "q_28",
         hasOptions: false,
         page:6,
         fieldsPerLine:1
@@ -355,7 +355,7 @@ const OutstandingInstForm= [
         title:"List the Workshops/ seminars/ conferences/ exhibitions conducted for employees and students if any (500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "workshop_seminars",
+        _name: "q_29",
         hasOptions: false,
         page:6,
         fieldsPerLine:1
@@ -364,7 +364,7 @@ const OutstandingInstForm= [
         title:"Elaborate on the participation in faculty exchange programmes (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "fac_exchng_prog",
+        _name: "q_30",
         hasOptions: false,
         page:7,
         fieldsPerLine:1
@@ -373,7 +373,7 @@ const OutstandingInstForm= [
         title:"State any Consultancy projects undertaken for Corporates or Government (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "consultancy_gov",
+        _name: "q_31",
         hasOptions: false,
         page:7,
         fieldsPerLine:1
@@ -382,7 +382,7 @@ const OutstandingInstForm= [
         title:"List the activities undertaken by students and teachers and report the outcomes of each.(500 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "stud_fac_acts",
+        _name: "q_32",
         hasOptions: false,
         page:8,
         fieldsPerLine:1
@@ -391,7 +391,7 @@ const OutstandingInstForm= [
         title:"List the key alumni events, with details of attendance numbers for each (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "alumini_events",
+        _name: "q_33",
         hasOptions: false,
         page:9,
         fieldsPerLine:1
@@ -400,7 +400,7 @@ const OutstandingInstForm= [
         title:"Involvement of alumni for guest lectures, conferences , as special guests, chief guests (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "alumini_envolvement",
+        _name: "q_34",
         hasOptions: false,
         page:9,
         fieldsPerLine:1
@@ -409,7 +409,7 @@ const OutstandingInstForm= [
         title:"What kind of support has been solicited / received from alumni eg donations and scholarships (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "support_scholarships",
+        _name: "q_35",
         hasOptions: false,
         page:9,
         fieldsPerLine:1
@@ -418,7 +418,7 @@ const OutstandingInstForm= [
         title:"Number of placements done based on the student intake (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "placements_done",
+        _name: "q_36",
         hasOptions: false,
         page:10,
         fieldsPerLine:1
@@ -427,7 +427,7 @@ const OutstandingInstForm= [
         title:"Provide details of activities undertaken and reports of outcomes (300 words only)",
         type:"textarea",
         requiredStatus : true,
-        _name: "svv_outcomes",
+        _name: "q_37",
         hasOptions: false,
         page:11,
         fieldsPerLine:1
@@ -436,7 +436,7 @@ const OutstandingInstForm= [
         title:"What are the significant contributions made by your institution to the support functions eg HR, IT, Sports, Security, PR, AR etc",
         type:"textarea",
         requiredStatus : true,
-        _name: "supports_contribs",
+        _name: "q_38",
         hasOptions: false,
         page:12,
         fieldsPerLine:1
