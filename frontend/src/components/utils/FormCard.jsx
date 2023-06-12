1dbdf59db9b30dd4b010a8cc94f6f710d27d00e5
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-06-12 09:36:51 +0530

    form cards updated

--- frontend/src/components/utils/FormCard.jsx
+++ frontend/src/components/utils/FormCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Sports from '../../container/assests/sports.jpeg';
+import {Link} from 'react-router-dom'
 
-const FormCard = () => {
+const FormCard = (props) => {
   const [hovered, setHovered] = useState(false);
 
   const handleHover = () => {
@@ -15,11 +15,11 @@ const FormCard = () => {
   return (
     <div className='flex justify-center'>
       <div
-        className='relative w-[70%] h-[20rem] shadow-lg bg-white rounded-sm'
+        className='relative w-[70%] h-[20rem] shadow-2xl bg-white rounded-sm'
         onMouseEnter={handleHover}
         onMouseLeave={handleMouseLeave}
       >
-        <img src={Sports} className='w-full h-full object-cover' alt='Sports' />
+        <img src={props.image} className='w-full h-full object-cover' alt='Sports' />
         <div
           id='card'
           className={`w-full ${hovered ? 'h-full opacity-80' : 'h-[8rem] opacity-50'} transition-all duration-500 rounded-t-lg bottom-0 absolute bg-black`}
@@ -27,24 +27,25 @@ const FormCard = () => {
           <div className='p-1'>
             <div>
               <h2 className='font-Poppins text-xl text-white ml-[5%] py-2'>
-                Sports
+                {props.title}
               </h2>
               <p className='text-sm font-Poppins text-white p-2'>
-                "Award form for sports: Honoring excellence in athletic achievements worldwide"
+                {props.info}
               </p>
 
-              {
-                hovered
-                ?
-                <>
-                    <div className=' relative w-full h-full'>
-                        <div className='absolute bg-blue-300 opacity-100'>
-                            Link
-                        </div>
-                    </div>
-                </>
-                : null
-              }
+            {
+              hovered
+              ?
+              <>
+               <Link to={props.link}>
+               <div className='text-black font-Poppins font-semibold bg-amber-600 hover:bg-amber-700 absolute bottom-0 right-0 m-10 p-3 rounded-lg text-center w-[6rem]  bg-opacity-100'>
+                  Link
+                </div>
+               </Link>
+              </>
+              :
+              null
+            }
             </div>
           </div>
         </div>
--- frontend/src/container/Pages/View Pages/FormCards.jsx
+++ frontend/src/container/Pages/View Pages/FormCards.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import FormCard from '../../../components/utils/FormCard'
-
+import Institution from '../../assests/institution.jpeg'
+import Teaching from '../../assests/teaching.jpeg'
+import NonTeaching from '../../assests/non-teaching.jpeg'
+import Research from '../../assests/research.jpeg'
+import Sports from '../../assests/sports.jpeg'
 
 const FormCards = () => {
     return (
-        <div className='p-5 w-full h-screen'>
+        <div className='p-5 w-full h-screen bg-[url("https://www.transparenttextures.com/patterns/washi.png")]'>
 
             {/* Headers  */}
             <div className='flex flex-col'>
@@ -13,7 +17,7 @@ const FormCards = () => {
                 </div>
                 <div className='my-1'>
                     <p className='text-md font-Poppins'>
-                        Forms ares shown as per eligibility criteria
+                        Forms are shown as per eligibility criteria of Institutions
                     </p>
                 </div>
             </div>
@@ -22,21 +26,46 @@ const FormCards = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-[5rem]  pb-[5rem] mt-[5rem] place-items-center'>
 
-            <div className=''>
-                <FormCard />
-            </div>
-            <div>
-                <FormCard />
-            </div>
-            <div>
-                <FormCard />
-            </div>
-            <div>
-                <FormCard />
-            </div>
-            <div>
-                <FormCard />
-            </div>
+                <div className=''>
+                    <FormCard
+                        image={Institution}
+                        title="Outstanding Institution"
+                        info="Celebrating excellence, impact, and remarkable achievements of outstanding institutions."
+                        link="/forms/outstanding-institution"
+                    />
+                </div>
+                <div>
+                    <FormCard
+                        image={Sports}
+                        title="Sports"
+                        info="Award form for sports: Honoring excellence in athletic achievements worldwide"
+                        link="/forms/sports"
+                    />
+                </div>
+                <div>
+                    <FormCard
+                        image={Research}
+                        title="Excellence in Research"
+                        info="Honoring excellence in research through prestigious and impactful awards."
+                        link="/forms/research"
+                    />
+                </div>
+                <div>
+                    <FormCard
+                        image={Teaching}
+                        title="Teaching"
+                        info="Recognizing teaching excellence with prestigious awards to inspiring educators"
+                        link="/forms/teaching"
+                    />
+                </div>
+                <div>
+                    <FormCard
+                        image={NonTeaching}
+                        title="Non Teaching"
+                        info="Commending administrative and staff excellence, driving institutional success and growth."
+                        link="/forms/non-teaching"
+                    />
+                </div>
             </div>
 
         </div>
