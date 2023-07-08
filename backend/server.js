c558016e92c74d25216d8a43ccafc61266271ccc
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-07-08 15:52:23 +0530

    added multiCore LoadBalancer

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,56 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const authRoute = require('./routes/authRoutes')
-const formRoute = require('./routes/formRoutes')
-const hoiRoutes = require('./routes/hoiRoutes')
-const ieacRoutes = require('./routes/ieacRoutes')
-const adminRoutes = require('./routes/adminRoutes')
-const errorHandler = require('./middleware/errorHandler')
-const cors = require('cors')
-const db = require('./models')
-const {serverLogger} = require('./middleware/logger')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const os = require('os');
+const cluster = require('cluster');
+const authRoute = require('./routes/authRoutes');
+const formRoute = require('./routes/formRoutes');
+const hoiRoutes = require('./routes/hoiRoutes');
+const ieacRoutes = require('./routes/ieacRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const db = require('./models');
+const { serverLogger } = require('./middleware/logger');
 
+const numCPUs = os.cpus().length;
 
-/**
- * creating express app 
- */
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-const app = express()
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-/**
- * Middleware
- */
-app.use(cors())
-app.use(express.json())
-app.use(express.static(`${__dirname}/data`));
-app.use('/auth', authRoute)
-app.use('/forms', formRoute )
-app.use('/hoi/data',hoiRoutes)
-app.use('/ieac/data',ieacRoutes)
-app.use('/admin/data',adminRoutes)
-app.use(errorHandler)
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
 
-/**
- * server listen and database configuration
- */
+  // creating express app
+  const app = express();
 
-db.sequelize.sync().then((req)=>{
-    serverLogger.info(`Connected to database ${req.config.database}`)
-    console.log("connected to mysql database ");
-})
+  // Middleware
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.static(`${__dirname}/data`));
+  app.use('/auth', authRoute);
+  app.use('/forms', formRoute);
+  app.use('/hoi/data', hoiRoutes);
+  app.use('/ieac/data', ieacRoutes);
+  app.use('/admin/data', adminRoutes);
+  app.use(errorHandler);
 
+  // server listen and database configuration
+  db.sequelize.sync().then((req) => {
+    serverLogger.info(`Connected to database ${req.config.database}`);
+    console.log('Connected to MySQL database');
+  });
 
-const PORT =  process.env.PORT || 5000
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , ()=>{
-    serverLogger.info(`Server started running at port ${PORT}`)
+  app.listen(PORT, () => {
+    serverLogger.info(`Server started running at port ${PORT}`);
     console.log(`Server started running at port ${PORT}`);
-})
+  });
+}
